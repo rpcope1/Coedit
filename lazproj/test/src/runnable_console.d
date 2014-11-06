0787af2b9d9b7a2f8b69cafb211d6e3f0e0d1953
Basile Burg <basile.burg@gmx.com>
2014-11-06 15:02:17 +0100

    added runnable console wrapper example

---
+++ lazproj/test/src/runnable_console.d
@@ -0,0 +1,25 @@
+module console;
+
+/*
+ The most simple console wrappr'.
+
+ usage:
+ ------
+ - Use Coedit menu, Compile and Run file to execute.
+ - use the "Process input widget to pass" commands.
+ - Don't forget that the Messages widget restricts the item count.
+ - type "exit" and press SEND to quit.
+*/
+void main(string args[])
+{
+    import std.process;
+    //
+    version(Windows)
+        string prgname = "cmd";
+    else
+        version(Linux) string prgname = "xterm";
+    else
+        assert(0, "unsupported target");
+    //
+    prgname.spawnProcess.wait;
+}
