14f6a7ceac11de63d6b908b4f07ba4d780b3c1fa
Basile Burg <basile.burg@gmx.com>
2014-11-18 22:35:51 +0100

    fix for the linux version, shortcut <CTRL>+<+> and <CTRL>+<-> were not working because of the default size (0).

--- src/ce_synmemo.pas
+++ src/ce_synmemo.pas
@@ -69,6 +69,7 @@ begin
   Font.Pitch := fpFixed;
   TabWidth := 4;
   BlockIndent := 4;
+  Font.Size:= 9;
   Options :=
     [eoAutoIndent, eoBracketHighlight, eoGroupUndo, eoTabsToSpaces,
     eoDragDropEditing, eoShowCtrlMouseLinks, eoEnhanceHomeKey, eoTabIndent];
