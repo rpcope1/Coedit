150fed87725b55d73ed8af2bfe993d23d8d9f3bd
Basile Burg <basile.burg@gmx.com>
2014-11-27 15:53:04 +0100

    added process input history, controlled with <UP> and <DOWN>

--- src/ce_procinput.pas
+++ src/ce_procinput.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  Menus, StdCtrls, ce_widget, process;
+  Menus, StdCtrls, ce_widget, process, ce_common;
 
 type
 
@@ -16,12 +16,16 @@ type
     txtInp: TEdit;
     txtExeName: TStaticText;
     procedure btnSendClick(Sender: TObject);
-    procedure txtInpKeyPress(Sender: TObject; var Key: char);
+    procedure txtInpKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
   private
+    fMruPos: Integer;
+    fMru: TMRUList;
     fProc: TProcess;
     procedure sendInput;
     procedure setProc(const aValue: TProcess);
   public
+    constructor create(aOwner: TComponent); override;
+    destructor destroy; override;
     property process: TProcess read fProc write setProc;
   end;
 
@@ -29,7 +33,20 @@ implementation
 {$R *.lfm}
 
 uses
-  ce_common, ce_symstring;
+  ce_symstring, LCLType;
+
+constructor TCEProcInputWidget.create(aOwner: TComponent);
+begin
+  inherited;
+  fMru := TMRUList.Create;
+  fMru.maxCount := 25;
+end;
+
+destructor TCEProcInputWidget.destroy;
+begin
+  fMru.Free;
+  inherited;
+end;
 
 procedure TCEProcInputWidget.setProc(const aValue: TProcess);
 begin
@@ -47,6 +64,8 @@ procedure TCEProcInputWidget.sendInput;
 var
   inp: string;
 begin
+  fMru.Insert(0,txtInp.Text);
+  fMruPos := 0;
   if txtInp.Text <> '' then
     inp := symbolExpander.get(txtInp.Text) + lineEnding
   else
@@ -55,20 +74,30 @@ begin
   txtInp.Text := '';
 end;
 
-procedure TCEProcInputWidget.txtInpKeyPress(Sender: TObject; var Key: char);
-begin
-  if fProc = nil then
-    exit;
-  if key <> #13 then
-    exit;
-  sendInput;
-end;
-
 procedure TCEProcInputWidget.btnSendClick(Sender: TObject);
 begin
   if fProc = nil then
     exit;
   sendInput;
+end;
+
+procedure TCEProcInputWidget.txtInpKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  case Key of
+    VK_RETURN:
+      if fProc <> nil then sendInput;
+    VK_UP: begin
+      fMruPos += 1;
+      if fMruPos > fMru.Count-1 then fMruPos := 0;
+      txtInp.Text := fMru.Strings[fMruPos];
+    end;
+    VK_DOWN: begin
+      fMruPos -= 1;
+      if fMruPos < 0 then fMruPos := fMru.Count-1;
+      txtInp.Text := fMru.Strings[fMruPos];
+    end;
+  end;
 end;
 
 end.
