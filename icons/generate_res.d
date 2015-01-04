d8bd4ad9ec8ec91a3903251987164a5b47790d1d
Basile Burg <basile.burg@gmx.com>
2015-01-04 01:57:00 +0100

    added shared icon system

---
+++ icons/generate_res.d
@@ -0,0 +1,23 @@
+// generate the Pascal code for using Coedit icon data at run-time.
+// usage: open in Coedit then click File, compile and run file.
+module generate_res;
+
+import std.file;
+import std.path;
+import std.string;
+import std.process;
+
+void main(string[] args)
+{
+    auto path = args[0].dirName;
+    string cmd[];
+    // resource compiler
+    cmd ~= "lazres";
+    // pascal source to include
+    cmd ~= ".." ~ dirSeparator ~ "src" ~ dirSeparator ~ "ce_icons.inc";
+    // resource items
+    foreach(f; dirEntries(path, "*.png", SpanMode.depth)) {
+        cmd ~= format("%s=%s",f.name, f.name.baseName.stripExtension);   
+    }
+    spawnProcess(cmd).wait;
+}
---
+++ src/ce_icons.pas
@@ -0,0 +1,16 @@
+unit ce_icons;
+
+{$I ce_defines.inc}
+
+interface
+
+uses
+  Classes, SysUtils, LResources;
+
+implementation
+
+
+initialization
+  {$I ce_icons.inc}
+end.
+
