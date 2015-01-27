rpcope1/Coedit

commit f4da130df1122f5bf586b4ffb3b40a1bd0555bf4

improved startup
widgets are not shown until docked

src/ce_main.pas
@@ -523,7 +523,7 @@ begin
 
   for i := 0 to fWidgList.Count-1 do
   begin
-    DockMaster.MakeDockable(fWidgList.widget[i],true);
+    DockMaster.MakeDockable(fWidgList.widget[i], false);
     DockMaster.GetAnchorSite(fWidgList.widget[i]).Header.HeaderPosition := adlhpTop;
   end;
 

commit 56299e965ffa64b51309ffad510d81abc49a31da

fix, bug introduced by f4da130df1122f5bf586b4ffb3b40a1bd0555bf4.
first launch, all widgets were hidden. The optimization is only set if the docking have been saved at least one.

src/ce_main.pas
@@ -523,7 +523,7 @@ begin
 
   for i := 0 to fWidgList.Count-1 do
   begin
-    DockMaster.MakeDockable(fWidgList.widget[i], false);
+    DockMaster.MakeDockable(fWidgList.widget[i], not fileExists(getCoeditDocPath + 'docking.xml'));
     DockMaster.GetAnchorSite(fWidgList.widget[i]).Header.HeaderPosition := adlhpTop;
   end;
 