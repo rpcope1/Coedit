f4da130df1122f5bf586b4ffb3b40a1bd0555bf4
Basile Burg <basile.burg@gmx.com>
2015-01-27 17:14:06 +0100

    improved startup
    widgets are not shown until docked

--- src/ce_main.pas
+++ src/ce_main.pas
@@ -523,7 +523,7 @@ begin
 
   for i := 0 to fWidgList.Count-1 do
   begin
-    DockMaster.MakeDockable(fWidgList.widget[i],true);
+    DockMaster.MakeDockable(fWidgList.widget[i], false);
     DockMaster.GetAnchorSite(fWidgList.widget[i]).Header.HeaderPosition := adlhpTop;
   end;
 
