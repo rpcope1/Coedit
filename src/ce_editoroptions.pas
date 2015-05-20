0c1862c9ba8db48207a442fa49e333cf2639733d
Basile Burg <basile.burg@gmx.com>
2015-05-20 16:13:07 +0200

    fix, editor option, canceled, not apllied

--- src/ce_editoroptions.pas
+++ src/ce_editoroptions.pas
@@ -391,10 +391,10 @@ begin
     self.Assign(fBackup);
     D2Syn.Assign(fBackup.fD2Syn);
     TxtSyn.Assign(fBackup.fTxtSyn);
-  end
+  end;
   // apply, if change/accept event
   // to get a live preview
-  else if anEvent <> oeeSelectCat then
+  if anEvent <> oeeSelectCat then
     applyChangesFromSelf;
   // new backup values based on accepted values.
   if anEvent = oeeAccept then
