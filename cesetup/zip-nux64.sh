4d69c60a5ec4a3a455ca65e2b841f2b8b5fe1a7d
Basile Burg <basile.burg@gmx.com>
2015-08-25 09:55:23 +0200

    put all the file for a release in a common folder

--- cesetup/zip-nux64.sh
+++ cesetup/zip-nux64.sh
@@ -1,5 +1,5 @@
 zip -9 -j \
-"coedit.1update2.linux32.zip" \
+"output/coedit.1update2.linux32.zip" \
 nux64/dcd.license.txt nux64/coedit.license.txt \
 nux64/coedit nux64/cetodo nux64/cesyms \
 nux64/coedit.ico nux64/coedit.png \
--- cesetup/zip-win32.bat
+++ cesetup/zip-win32.bat
@@ -1,7 +1,7 @@
 cd win32
 :: assuming 7zip binary folder is somewhere in PATH
 7z a -tzip -mx9^
- ..\coedit.1update2.win32.zip^
+ ..\output\coedit.1update2.win32.zip^
  dcd.license.txt coedit.license.txt^
  coedit.exe cetodo.exe cesyms.exe^
  coedit.ico coedit.png^
