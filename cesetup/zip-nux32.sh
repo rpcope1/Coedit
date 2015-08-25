b877ce5f08c865d7a42bfdc52cc0cb1275947430
Basile Burg <basile.burg@gmx.com>
2015-08-25 08:41:00 +0200

    added linux scripts to make the zip files

---
+++ cesetup/zip-nux32.sh
@@ -0,0 +1,6 @@
+zip -9 -j \
+"coedit.1update2.linux32.zip" \
+nux32/dcd.license.txt nux32/coedit.license.txt \
+nux32/coedit nux32/cetodo nux32/cesyms \
+nux32/coedit.ico nux32/coedit.png \
+nux32/dcd-server nux32/dcd-client
---
+++ cesetup/zip-nux64.sh
@@ -0,0 +1,6 @@
+zip -9 -j \
+"coedit.1update2.linux32.zip" \
+nux64/dcd.license.txt nux64/coedit.license.txt \
+nux64/coedit nux64/cetodo nux64/cesyms \
+nux64/coedit.ico nux64/coedit.png \
+nux64/dcd-server nux64/dcd-client
