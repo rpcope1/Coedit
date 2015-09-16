rpcope1/Coedit

commit bcf479b3fe41eb1b3a546c555241fc8b73dff9a4

symbol list, write source to input rather than using tmp file

cesyms/cesyms.d
@@ -6,19 +6,28 @@ import std.traits;
 
 void main(string[] args)
 {
-    if (args.length < 2) return;
-    auto fname = args[1];
-    if (!fname.exists) return;
+    // get either the module from stdin or from first arg
+    string fname;
+    ubyte[] source;
+    if (args.length == 1)
+    {
+        source.length = cast(size_t)stdin.size;
+        source = stdin.rawRead(source);
+    }
+    else
+    {
+        fname = args[1];
+        if (!fname.exists) return;
+        source = cast(ubyte[]) read(fname, size_t.max);
+    }
 
     // load and parse the file
     auto config = LexerConfig(fname, StringBehavior.source, WhitespaceBehavior.skip);
-    auto source = cast(ubyte[]) read(fname, size_t.max);
     auto scache = StringCache(StringCache.defaultBucketCount);
-    
     auto ast = parseModule(getTokensForParser(source, config, &scache), fname, null, &(SymbolListBuilder.astError));
 
     // visit each root member
-    auto slb = construct!SymbolListBuilder; 
+    SymbolListBuilder slb = construct!SymbolListBuilder;
     foreach(Declaration decl; ast.declarations)
     {
         slb.resetRoot;

src/ce_symlist.pas
@@ -652,14 +652,13 @@ end;
 
 procedure TCESymbolListWidget.callToolProc;
 var
-  srcFname: string;
+  str: string;
 begin
   if not fHasToolExe then exit;
   if fDoc = nil then exit;
   if fDoc.Lines.Count = 0 then exit;
   if not fDoc.isDSource then exit;
-
-  // standard process options
+  //
   killProcess(fToolProc);
   fToolProc := TCEProcess.Create(nil);
   fToolProc.ShowWindow := swoHIDE;
@@ -667,15 +666,10 @@ begin
   fToolProc.Executable := exeFullName(toolExeName);
   fToolProc.OnTerminate := @toolTerminated;
   fToolProc.CurrentDirectory := ExtractFileDir(Application.ExeName);
-
-  // focused source
-  srcFname := fDoc.fileName;
-  if not fileExists(srcFname) or (srcFname = fDoc.tempFilename) then
-    fDoc.saveTempFile;
-  srcFname := fDoc.fileName;
-  fToolProc.Parameters.Add(srcFname);
-
   fToolProc.Execute;
+  str := fDoc.Text;
+  fToolProc.Input.Write(str[1], length(str));
+  fToolProc.CloseInput;
 end;
 
 procedure TCESymbolListWidget.toolTerminated(sender: TObject);

src/ce_symlist.lfm
@@ -24,6 +24,7 @@ inherited CESymbolListWidget: TCESymbolListWidget
         Width = 302
         Align = alClient
         BorderSpacing.Around = 4
+        DefaultItemHeight = 18
         HideSelection = False
         Images = imgList
         ReadOnly = True

commit 612b30c1a58a3e596670c592e9bb313079df416d

fix, symbol list broken under linux since bcf479b3fe41eb1b3a546c555241fc8b73dff9a4, due to not input stream size

cesyms/cesyms.d
@@ -11,8 +11,8 @@ void main(string[] args)
     ubyte[] source;
     if (args.length == 1)
     {
-        source.length = cast(size_t)stdin.size;
-        source = stdin.rawRead(source);
+        foreach(buff; stdin.byChunk(1024))
+            source ~= buff;
     }
     else
     {

src/ce_symlist.pas
@@ -663,7 +663,7 @@ begin
   fToolProc := TCEProcess.Create(nil);
   fToolProc.ShowWindow := swoHIDE;
   fToolProc.Options := [poUsePipes];
-  fToolProc.Executable := exeFullName(toolExeName);
+  fToolProc.Executable := toolExeName;
   fToolProc.OnTerminate := @toolTerminated;
   fToolProc.CurrentDirectory := ExtractFileDir(Application.ExeName);
   fToolProc.Execute;
@@ -741,6 +741,7 @@ begin
   fToolProc.OnTerminate := nil;
   fToolProc.OnReadData  := nil;
   fToolProc.OutputStack.Position:=0;
+  if fToolProc.OutputStack.Size = 0 then exit;
   fSyms.LoadFromTool(fToolProc.OutputStack);
   //
   tree.BeginUpdate;