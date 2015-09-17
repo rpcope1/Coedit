546052732fbd8e9e13c31520973ebe68be4aad24
Basile Burg <basile.burg@gmx.com>
2015-09-17 18:16:58 +0200

    allow more ctrl types to be assignedto

--- src/ce_sharedres.pas
+++ src/ce_sharedres.pas
@@ -3,23 +3,25 @@ unit ce_sharedres;
 interface
 
 uses
-  LResources, Controls, Buttons, Graphics;
+  LResources, Classes, Controls, Buttons, Graphics;
 
-procedure AssignPng(ctrl: TControl; const resName: string);
+procedure AssignPng(ctrl: TPersistent; const resName: string);
 
 implementation
 
 var
   png: TPortableNetworkGraphic;
 
-procedure AssignPng(ctrl: TControl; const resName: string);
+procedure AssignPng(ctrl: TPersistent; const resName: string);
 begin
   try
     png.LoadFromLazarusResource(resName);
     if ctrl is TCustomBitBtn then
       TCustomBitBtn(ctrl).Glyph.Assign(png)
     else if ctrl is TCustomSpeedButton then
-      TCustomSpeedButton(ctrl).Glyph.Assign(png);
+      TCustomSpeedButton(ctrl).Glyph.Assign(png)
+    else if ctrl is TBitmap then
+      TBitmap(ctrl).Assign(png);
   except
   end;
 end;
