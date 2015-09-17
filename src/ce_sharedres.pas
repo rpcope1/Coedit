fd54d7c12acc211d86d81ac4308c48f2bdd48dc4
Basile Burg <basile.burg@gmx.com>
2015-09-17 06:46:04 +0200

    added util function to load shared res to btn

--- src/ce_sharedres.pas
+++ src/ce_sharedres.pas
@@ -3,9 +3,31 @@ unit ce_sharedres;
 interface
 
 uses
-  LResources;
+  LResources, Controls, Buttons, Graphics;
+
+procedure AssignPng(ctrl: TControl; const resName: string);
 
 implementation
+
+procedure AssignPng(ctrl: TControl; const resName: string);
+var
+  png : TPortableNetworkGraphic;
+begin
+  png := TPortableNetworkGraphic.Create;
+  try
+    try
+      png.LoadFromLazarusResource(resName);
+      if ctrl is TCustomBitBtn then
+        TCustomBitBtn(ctrl).Glyph.Assign(png)
+      else if ctrl is TCustomSpeedButton then
+        TCustomSpeedButton(ctrl).Glyph.Assign(png);
+    except
+    end;
+  finally
+    png.Free;
+  end;
+
+end;
 
 initialization
   {$I ../src/ce_icons.inc}
