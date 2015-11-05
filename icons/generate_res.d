89675d2c5719e1b05cc6bd4cb3a98eeee4f8e6cf
Basile Burg <basile.b@gmx.com>
2015-11-05 02:26:19 +0100

    updated shared icons

--- icons/generate_res.d
+++ icons/generate_res.d
@@ -7,12 +7,15 @@ import std.path;
 import std.string;
 import std.process;
 
+version(linux) string pathto = "/usr/lib64/lazarus/tools/";
+else string pathto = "";
+
 void main(string[] args)
 {
     auto path = args[0].dirName;
     string[] cmd;
     // resource compiler
-    cmd ~= "lazres";
+    cmd ~= pathto ~ "lazres";
     // pascal source to include
     cmd ~= ".." ~ dirSeparator ~ "src" ~ dirSeparator ~ "ce_icons.inc";
     // resource items
