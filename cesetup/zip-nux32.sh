d92eb999ee32bb23b15c9fa62922fa1b5d2677b5
Basile Burg <basile.b@gmx.com>
2016-02-07 20:52:58 +0100

    setup, zip nix files in a sub dir, close #11

--- cesetup/zip-nux32.sh
+++ cesetup/zip-nux32.sh
@@ -1,8 +1,12 @@
 ver=`cat version.txt`
+fld=coedit-x86
 cd nux32
-zip -9 -j \
+mkdir $fld/
+cp * $fld/
+zip -9 \
 ../output/coedit.$ver.linux32.zip \
-dcd.license.txt coedit.license.txt \
-coedit cetodo cesyms \
-coedit.ico coedit.png \
-dcd-server dcd-client
+$fld/dcd.license.txt $fld/coedit.license.txt \
+$fld/coedit $fld/cetodo $fld/cesyms \
+$fld/coedit.ico $fld/coedit.png \
+$fld/dcd-server $fld/dcd-client
+rm -rf coedit-x86
--- cesetup/zip-nux64.sh
+++ cesetup/zip-nux64.sh
@@ -1,8 +1,12 @@
 ver=`cat version.txt`
+fld=coedit-x86_64
 cd nux64
-zip -9 -j \
+mkdir $fld/
+cp * $fld/
+zip -9 \
 ../output/coedit.$ver.linux64.zip \
-dcd.license.txt coedit.license.txt \
-coedit cetodo cesyms \
-coedit.ico coedit.png \
-dcd-server dcd-client
+$fld/dcd.license.txt $fld/coedit.license.txt \
+$fld/coedit $fld/cetodo $fld/cesyms \
+$fld/coedit.ico $fld/coedit.png \
+$fld/dcd-server $fld/dcd-client
+rm -rf coedit-x86_64
